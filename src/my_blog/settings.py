9e2c66be2addbe59deae0090787f811033bd8355
OUAZIN Nabil <nabilouazin91@gmail.com>
2022-12-14 18:49:03 +0100

    somechanges

--- src/my_blog/settings.py
+++ src/my_blog/settings.py
@@ -120,6 +120,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+# If you want to make static folder for all project
+#STATICFILES_DIRS = [
+#        BASE_DIR / 'static',
+#]
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
